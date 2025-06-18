f32982d61b472beddb5481aaeb675eeb83a7a6a2
Štefan Fabian <stefafabian@gmail.com>
2025-06-18 12:18:03 +0200

    drobne upravy layutu bannera

--- klient-frontend/src/layout/MainLayout.tsx
+++ klient-frontend/src/layout/MainLayout.tsx
@@ -5,12 +5,12 @@ export default function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen w-screen">
       
-      <div className="w-full max-w-screen px-40">
+      <div className="w-full max-w-screen px-60">
         <TopNav />
       </div>
 
-      <div className="flex-1 w-full flex justify-center px-40">
-        <div className="w-full max-w-screen">
+      <div className="flex-1 w-full flex justify-center px-60">
+        <div className="w-full max-w-screen px-8">
           <Outlet />
         </div>
       </div>
